68584f91fae43ebd1711b60a62ccce550a2103a4
Nicholas <nicholasmoskalenko@gmail.com>
2023-03-09 21:24:42 +0200

    add second task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,17 +4,37 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const galleryContainer = document.querySelector(".gallery");
-const murkupForGalleryItems = galleryItems
+const markupForGalleryItems = galleryItems
   .map(({ preview, original, description }) => {
     return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="large-image.jpg"
+      data-source="${original}"
       alt="${description}"
     />
   </a>
 </div>`;
   })
   .join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", markupForGalleryItems);
+
+galleryContainer.addEventListener("click", handleLinkClick);
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  const instanse = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instanse.show();
+  galleryContainer.addEventListener("keydown", modalClose);
+}
+
+function modalClose(event) {
+  console.log(event.code);
+  if (event.code === "Escape") {
+    instanse.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const markupForGalleryItems = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+  })
+  .join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", markupForGalleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+lightbox.on("show.simplelightbox");
